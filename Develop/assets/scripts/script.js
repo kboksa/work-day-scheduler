b700fdadf0474bc39086cf82e1de8cc040c44dbf
kalina <kalina.boksa@gmail.com>
2022-08-17 14:00:01 -0500

    add text to typing area

--- Develop/assets/scripts/script.js
+++ Develop/assets/scripts/script.js
@@ -5,6 +5,7 @@ THEN the current day is displayed at the top of the calendar*/
 var todayDate = moment().format("MMM. Do YYYY");
 $("#currentDay").html(todayDate);
 
+var hourNow = moment().minutes(0).seconds(0).milliseconds(0);
 /*WHEN I scroll down
 THEN I am presented with timeblocks for standard business hours*/
 var hourLength = 9;
@@ -48,10 +49,22 @@ for (let i = 0; i < hourLength; i++) {
 /*WHEN I view the timeblocks for that day
 THEN each timeblock is color coded to indicate whether it is in the past,
  present, or future*/
+let hourStart = moment().hour(8).minutes(0).seconds(0).milliseconds(0);
+
+for (let i = 0; i < hour.length; i++) {
+  let hourBlock = hourStart.add(1, "h");
+  if (hourBlock.isBefore(hourNow)) {
+    $(eventColumn[i]).addClass("past");
+  } else if (hourBlock.isSame(hourNow)) {
+    $(eventColumn[i]).addClass("present");
+  } else {
+    $(evenColumn[i]).addClass("futrue");
+  }
+}
 
 /*WHEN I click into a timeblock
  THEN I can enter an event*/
-
+var userMessage = $(".textarea");
 /*WHEN I click the save button for that timeblock
 THEN the text for that event is saved in local storage*/
 
