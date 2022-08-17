5227aa8c4002e847b5ba63cc06a4cdafdf13c58f
kalina <kalina.boksa@gmail.com>
2022-08-17 18:30:04 -0500

    fixed time interval

--- Develop/assets/scripts/script.js
+++ Develop/assets/scripts/script.js
@@ -2,17 +2,27 @@
 
 /*WHEN I open the planner
 THEN the current day is displayed at the top of the calendar*/
-var todayDate = moment().format("MMM. Do YYYY");
-$("#currentDay").html(todayDate);
+// var todayDate = moment().format("MMM. Do YYYY");
+// $("#currentDay").html(todayDate);
 
-var hourNow = moment().minutes(0).seconds(0).milliseconds(0);
+$(".jumbotron").append($("<p>")).addClass("par");
+
+let interval = setInterval(() => {
+  let currentTime = dayjs().format("hh:mm:ss a");
+  let currentDay = dayjs().format("MMMM DD YYYY");
+  $(".par").text(`${currentTime} ${currentDay}`);
+}, 1000);
+
+let currentHour = parseInt(dayjs().format("HH"));
+console.log(currentHour);
+// var hourNow = moment().minutes(0).seconds(0).milliseconds(0);
 /*WHEN I scroll down
 THEN I am presented with timeblocks for standard business hours*/
 var hourLength = 9;
 var hour = $(".hour");
-var save = $(".saveBtn");
+var saveBtn = $(".saveBtn");
 var eventColumn = $(".time-block");
-var save = $(".deleteBtn");
+var deleteBtn = $(".deleteBtn");
 var userMessage = $(".textarea");
 
 for (let i = 0; i < hourLength; i++) {
@@ -24,7 +34,7 @@ for (let i = 0; i < hourLength; i++) {
   hour.text(i + hourLength + ":00");
 
   var userMessage = $("<textarea>");
-  userMessage.addClass("textarea col-9 description");
+  userMessage.addClass("textarea col-9 description").attr("id", i + 9);
 
   var saveBtn = $("<button>");
   saveBtn.addClass("saveBtn col-1");
@@ -46,21 +56,33 @@ for (let i = 0; i < hourLength; i++) {
   eventColumn.append(deleteBtn);
   $(".container").append(eventColumn);
 }
+
+$(".textarea").each(function () {
+  console.log(parseInt($(this).attr("id")));
+  if (parseInt($(this).attr("id")) < currentHour) {
+    $(this).addClass("past");
+  } else if (parseInt($(this).attr("id")) == currentHour) {
+    $(this).addClass("present");
+  } else {
+    $(this).addClass("future");
+  }
+});
+
 /*WHEN I view the timeblocks for that day
 THEN each timeblock is color coded to indicate whether it is in the past,
  present, or future*/
-let hourStart = moment().hour(8).minutes(0).seconds(0).milliseconds(0);
+// let hourStart = moment().hour(8).minutes(0).seconds(0).milliseconds(0);
 
-for (let i = 0; i < hour.length; i++) {
-  let hourBlock = hourStart.add(1, "h");
-  if (hourBlock.isBefore(hourNow)) {
-    $(eventColumn[i]).addClass("past");
-  } else if (hourBlock.isSame(hourNow)) {
-    $(eventColumn[i]).addClass("present");
-  } else {
-    $(evenColumn[i]).addClass("futrue");
-  }
-}
+// for (let i = 0; i < hour.length; i++) {
+//   let hourBlock = hourStart.add(1, "h");
+//   if (hourBlock.isBefore(hourNow)) {
+//     $(eventColumn[i]).addClass("past");
+//   } else if (hourBlock.isSame(hourNow)) {
+//     $(eventColumn[i]).addClass("present");
+//   } else {
+//     $(evenColumn[i]).addClass("futrue");
+//   }
+// }
 
 /*WHEN I click into a timeblock
  THEN I can enter an event*/
