b0fe2996173de4d8d2c1e5e3dfc1c7a0b516f315
kalina <kalina.boksa@gmail.com>
2022-08-16 23:04:15 -0500

    added colums

--- Develop/assets/scripts/script.js
+++ Develop/assets/scripts/script.js
@@ -15,8 +15,35 @@ var save = $(".deleteBtn");
 var userMessage = $(".textarea");
 
 for (let i = 0; i < hourLength; i++) {
-  var eventColumn = $("div");
+  var eventColumn = $("<div>");
   eventColumn.addClass("row time-block");
+
+  var hour = $("<div>");
+  hour.addClass("hour col-1");
+  hour.text(i + hourLength + ":00");
+
+  var userMessage = $("<textarea>");
+  userMessage.addClass("textarea col-9 description");
+
+  var saveBtn = $("<button>");
+  saveBtn.addClass("saveBtn col-1");
+
+  var iconSave = $("<i>");
+  iconSave.addClass("far fa-save");
+
+  var deleteBtn = $("<button>");
+  deleteBtn.addClass("deleteBtn col-1");
+
+  var iconDelete = $("<i>");
+  iconDelete.addClass("fas fa-trash-alt");
+
+  saveBtn.append(iconSave);
+  deleteBtn.append(iconDelete);
+  eventColumn.append(hour);
+  eventColumn.append(userMessage);
+  eventColumn.append(saveBtn);
+  eventColumn.append(deleteBtn);
+  $(".container").append(eventColumn);
 }
 /*WHEN I view the timeblocks for that day
 THEN each timeblock is color coded to indicate whether it is in the past,
