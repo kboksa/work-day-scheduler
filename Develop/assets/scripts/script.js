9dc41cea106c182464cd1c1f5bf35584c2262dee
kalina <kalina.boksa@gmail.com>
2022-08-17 23:55:45 -0500

    add storage

--- Develop/assets/scripts/script.js
+++ Develop/assets/scripts/script.js
@@ -2,8 +2,6 @@
 
 /*WHEN I open the planner
 THEN the current day is displayed at the top of the calendar*/
-// var todayDate = moment().format("MMM. Do YYYY");
-// $("#currentDay").html(todayDate);
 
 $(".jumbotron").append($("<p>").addClass("par"));
 
@@ -15,7 +13,6 @@ let interval = setInterval(() => {
 
 let currentHour = parseInt(dayjs().format("HH"));
 console.log(currentHour);
-// var hourNow = moment().minutes(0).seconds(0).milliseconds(0);
 /*WHEN I scroll down
 THEN I am presented with timeblocks for standard business hours*/
 var hourLength = 9;
@@ -71,24 +68,36 @@ $(".textarea").each(function () {
 /*WHEN I view the timeblocks for that day
 THEN each timeblock is color coded to indicate whether it is in the past,
  present, or future*/
-// let hourStart = moment().hour(8).minutes(0).seconds(0).milliseconds(0);
-
-// for (let i = 0; i < hour.length; i++) {
-//   let hourBlock = hourStart.add(1, "h");
-//   if (hourBlock.isBefore(hourNow)) {
-//     $(eventColumn[i]).addClass("past");
-//   } else if (hourBlock.isSame(hourNow)) {
-//     $(eventColumn[i]).addClass("present");
-//   } else {
-//     $(evenColumn[i]).addClass("futrue");
-//   }
-// }
 
 /*WHEN I click into a timeblock
  THEN I can enter an event*/
 var userMessage = $(".textarea");
 /*WHEN I click the save button for that timeblock
 THEN the text for that event is saved in local storage*/
+function saveInfo(n) {
+  $(saveBtn[n]).on("click", function (event) {
+    event.preventDefault();
+
+    var message = $(userMessage[n]).val();
+    if (message !== "") {
+      localStorage.setItem("Todo-" + (n + hour.length) + ":00", message);
+    }
+  });
+}
+
+function deleteInfo(n) {
+  $(deleteBtn[n]).on("click", function (event) {
+    event.preventDefault();
+    localStorage.removeItem("Todo-" + (n + hour.length) + ":00");
+    $(userMessage[n]).text("");
+  });
+}
 
 /*WHEN I refresh the page
 THEN the saved events persist*/
+for (var i = 0; i < hour.length; i++) {
+  var userInput = localStorage.getItem("Todo-" + (i + hour.length) + ":00");
+  $(userMessage[i]).text(userInput);
+  saveInfo(i);
+  deleteInfo(i);
+}
